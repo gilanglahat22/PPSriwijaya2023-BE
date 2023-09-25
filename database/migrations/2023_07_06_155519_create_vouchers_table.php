1b6f3ac5bdf5bd36c3ba7987fbf3f25555c3b847
gilanglahat22 <gilanglahat43@gmail.com>
2023-09-26 01:00:09 +0700

    add voucher seeder

--- database/migrations/2023_07_06_155519_create_vouchers_table.php
+++ database/migrations/2023_07_06_155519_create_vouchers_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('vouchers', function (Blueprint $table) {
             $table->id();
             $table->string('kode_voucher');
+            $table->integer('nominal');
             $table->boolean('is_used')->default(false);
             $table->timestamps();
         });
--- database/seeders/VoucherSeeder.php
+++ database/seeders/VoucherSeeder.php
@@ -13,9 +13,54 @@ class VoucherSeeder extends Seeder
      */
     public function run(): void
     {
-        Voucher::create([
-            'kode_voucher' => "ahgsdyasgdasgd",
-            'is_used' => false,
-        ]);
+        for($i=0; $i<1000; $i++){
+            Voucher::create([
+                'kode_voucher' => $this->generateRandomString(10),
+                'nominal' => "10",
+                'is_used' => false,
+            ]);
+        }
+
+        for($i=0; $i<500; $i++){
+            Voucher::create([
+                'kode_voucher' => $this->generateRandomString(10),
+                'nominal' => "25",
+                'is_used' => false,
+            ]);
+        }
+
+        for($i=0; $i<500; $i++){
+            Voucher::create([
+                'kode_voucher' => $this->generateRandomString(10),
+                'nominal' => "50",
+                'is_used' => false,
+            ]);
+        }
+
+        for($i=0; $i<250; $i++){
+            Voucher::create([
+                'kode_voucher' => $this->generateRandomString(10),
+                'nominal' => "100",
+                'is_used' => false,
+            ]);
+        }
+
+        for($i=0; $i<100; $i++){
+            Voucher::create([
+                'kode_voucher' => $this->generateRandomString(10),
+                'nominal' => "500",
+                'is_used' => false,
+            ]);
+        }
+    }
+
+    public function generateRandomString($length = 25) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ%^&*()!@#$~{}[]?<>';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
     }
 }
